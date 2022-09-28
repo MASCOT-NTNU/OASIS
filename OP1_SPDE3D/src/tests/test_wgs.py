cf9a6abe39f7ae2abc3611c699b8a9c15a02cb97
Yaolin Ge <geyaolin@gmail.com>
2022-09-28 15:48:04 +0200

    cleaned_repo

---
+++ OP1_SPDE3D/src/tests/test_wgs.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from WGS import WGS
+
+
+class TestWGS(TestCase):
+
+    def setUp(self) -> None:
+        self.wgs = WGS()
+
+    def test_origin(self):
+        pass
+
