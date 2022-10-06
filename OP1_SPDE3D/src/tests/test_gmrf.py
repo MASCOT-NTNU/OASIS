d010399737a03180322e8600a85baa90dc617c5f
Yaolin Ge <geyaolin@gmail.com>
2022-10-06 14:42:26 +0100

    fix_gmrf

--- OP1_SPDE3D/src/tests/test_gmrf.py
+++ OP1_SPDE3D/src/tests/test_gmrf.py
@@ -26,140 +26,183 @@ class TestGMRF(TestCase):
         self.gmrf = GMRF()
         self.grid = self.gmrf.get_gmrf_grid()
 
-    def test_get_spde_grid(self) -> None:
-        self.gmrf.construct_gmrf_grid()
+    # def test_get_spde_grid(self) -> None:
+    #     self.gmrf.construct_gmrf_grid()
+    #
+    # def test_get_ind_from_location(self) -> None:
+    #     """
+    #     Test if given a location, it will return the correct index in GMRF grid.
+    #     """
+    #     # c1: one location
+    #     ide = 10
+    #     loc = self.gmrf.get_location_from_ind(ide)
+    #     id = self.gmrf.get_ind_from_location(loc)
+    #     self.assertEqual(ide, id)
+    #
+    #     # c2: more locations
+    #     ide = [10, 12]
+    #     loc = self.gmrf.get_location_from_ind(ide)
+    #     id = self.gmrf.get_ind_from_location(loc)
+    #     self.assertIsNone(testing.assert_array_equal(ide, id))
+    #
+    # def test_get_ibv(self) -> None:
+    #     """
+    #     Test if GMRF is able to compute IBV
+    #     """
+    #     # c1: mean at threshold
+    #     threshold = 0
+    #     mu = np.array([0])
+    #     sigma_diag = np.array([1])
+    #     ibv = self.gmrf.get_ibv(threshold, mu, sigma_diag)
+    #     self.assertEqual(ibv, .25)
+    #
+    #     # c2: mean further away from threshold
+    #     threshold = 0
+    #     mu = np.array([3])
+    #     sigma_diag = np.array([1])
+    #     ibv = self.gmrf.get_ibv(threshold, mu, sigma_diag)
+    #     self.assertLess(ibv, .01)
+    #
+    # def test_get_eibv_at_locations(self) -> None:
+    #     """
+    #     Test if it can return eibv for the given locations.
+    #     """
+    #     # id = [1, 2, 3, 4, 5]
+    #     id = np.random.randint(0, 1000, 5)
+    #     loc = self.gmrf.get_location_from_ind(id)
+    #     eibv = self.gmrf.get_eibv_at_locations(loc)
+    #     self.assertIsNotNone(eibv)
 
-    def test_get_ind_from_location(self) -> None:
-        """
-        Test if given a location, it will return the correct index in GMRF grid.
-        """
-        # c1: one location
-        ide = 10
-        loc = self.gmrf.get_location_from_ind(ide)
-        id = self.gmrf.get_ind_from_location(loc)
-        self.assertEqual(ide, id)
+    def test_check_assimilation(self):
+        print("hello world")
+        x_start = 1000
+        y_start = 500
+        z_start = -.5
+        x_end = 1000
+        y_end = 1500
+        z_end = -4.5
+        N = 20
+        x = np.linspace(x_start, x_end, N)
+        y = np.linspace(y_start, y_end, N)
+        z = np.linspace(z_start, z_end, N)
+        dataset = np.vstack((x, y, z, np.zeros_like(z))).T
+        ind, value, idm = self.gmrf.assimilate_data(dataset)
+        print("ind: ", ind)
 
-        # c2: more locations
-        ide = [10, 12]
-        loc = self.gmrf.get_location_from_ind(ide)
-        id = self.gmrf.get_ind_from_location(loc)
-        self.assertIsNone(testing.assert_array_equal(ide, id))
+        fig = go.Figure(data=go.Scatter3d(
+            x=self.grid[:, 1],
+            y=self.grid[:, 0],
+            z=self.grid[:, 2],
+            mode='markers',
+            marker=dict(color='black', size=1, opacity=.5)
+        ))
+        fig.add_trace(go.Scatter3d(
+            x=y,
+            y=x,
+            z=z,
+            mode='lines+markers',
+            marker=dict(color='red', size=5, opacity=.5),
+            line=dict(color='red', width=4)
+        ))
+        fig.add_trace(go.Scatter3d(
+            x=self.grid[ind, 1],
+            y=self.grid[ind, 0],
+            z=self.grid[ind, 2],
+            mode='markers',
+            marker=dict(color='blue', size=10, opacity=.5, symbol="square")
+        ))
+        fig.update_layout(scene_aspectmode='manual',
+                          scene_aspectratio=dict(x=1, y=1, z=.5))
 
-    def test_get_ibv(self) -> None:
-        """
-        Test if GMRF is able to compute IBV
-        """
-        # c1: mean at threshold
-        threshold = 0
-        mu = np.array([0])
-        sigma_diag = np.array([1])
-        ibv = self.gmrf.get_ibv(threshold, mu, sigma_diag)
-        self.assertEqual(ibv, .25)
+        plotly.offline.plot(fig, filename="/Users/yaolin/Downloads/check_assimilation.html", auto_open=True)
 
-        # c2: mean further away from threshold
-        threshold = 0
-        mu = np.array([3])
-        sigma_diag = np.array([1])
-        ibv = self.gmrf.get_ibv(threshold, mu, sigma_diag)
-        self.assertLess(ibv, .01)
+    # def test_assimilate_data(self):
+    #     """
+    #     Test if it can assimilate data with given dataset.
+    #     - 100 grid points within the grid.
+    #     - 10 replicates with 10 grid points not within the grid.
+    #     - no location.
+    #     """
+    #     # c1: grid points on grid
+    #     ind = np.random.randint(0, self.grid.shape[0], 100)
+    #     x = self.grid[ind, 0]
+    #     y = self.grid[ind, 1]
+    #     z = self.grid[ind, 2]
+    #     v = np.zeros_like(z)
+    #     dataset = np.stack((x, y, z, v), axis=1)
+    #     ida, sal_a, ind_min = self.gmrf.assimilate_data(dataset)
+    #     id = np.where(np.abs(z) <= -.25)[0]
+    #     dx = self.grid[ind_min, 0] - x[id]
+    #     dy = self.grid[ind_min, 1] - y[id]
+    #     dz = self.grid[ind_min, 2] - z[id]
+    #     gap = np.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
+    #     self.assertLess(np.amax(gap), 100)
+    #
+    #     # c2: random locations
+    #     for i in range(10):
+    #         ind = np.random.randint(0, self.grid.shape[0], 10)
+    #         x = self.grid[ind, 0] + np.random.randn(len(ind))
+    #         y = self.grid[ind, 1] + np.random.randn(len(ind))
+    #         z = self.grid[ind, 2] + np.random.randn(len(ind))
+    #         v = np.zeros_like(z)
+    #         dataset = np.stack((x, y, z, v), axis=1)
+    #         ida, sal_a, ind_min = self.gmrf.assimilate_data(dataset)
+    #         id = np.where(np.abs(z) <= -.25)[0]
+    #         dx = self.grid[ind_min, 0] - x[id]
+    #         dy = self.grid[ind_min, 1] - y[id]
+    #         dz = self.grid[ind_min, 2] - z[id]
+    #         gap = np.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
+    #         self.assertLess(np.amax(gap), 100)
+    #
+    #     # c3: no location
+    #     dataset = np.empty([0, 4])
+    #     ida, sal_a, ind_min = self.gmrf.assimilate_data(dataset)
+    #     self.assertTrue([True if len(ida) == 0 else False])
 
-    def test_get_eibv_at_locations(self) -> None:
-        """
-        Test if it can return eibv for the given locations.
-        """
-        # id = [1, 2, 3, 4, 5]
-        id = np.random.randint(0, 1000, 5)
-        loc = self.gmrf.get_location_from_ind(id)
-        eibv = self.gmrf.get_eibv_at_locations(loc)
-        self.assertIsNotNone(eibv)
-
-    def test_assimilate_data(self):
-        """
-        Test if it can assimilate data with given dataset.
-        - 100 grid points within the grid.
-        - 10 replicates with 10 grid points not within the grid.
-        - no location.
-        """
-        # c1: grid points on grid
-        ind = np.random.randint(0, self.grid.shape[0], 100)
-        x = self.grid[ind, 0]
-        y = self.grid[ind, 1]
-        z = self.grid[ind, 2]
-        v = np.zeros_like(z)
-        dataset = np.stack((x, y, z, v), axis=1)
-        ida, sal_a, ind_min = self.gmrf.assimilate_data(dataset)
-        id = np.where(np.abs(z) <= -.25)[0]
-        dx = self.grid[ind_min, 0] - x[id]
-        dy = self.grid[ind_min, 1] - y[id]
-        dz = self.grid[ind_min, 2] - z[id]
-        gap = np.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
-        self.assertLess(np.amax(gap), 100)
-
-        # c2: random locations
-        for i in range(10):
-            ind = np.random.randint(0, self.grid.shape[0], 10)
-            x = self.grid[ind, 0] + np.random.randn(len(ind))
-            y = self.grid[ind, 1] + np.random.randn(len(ind))
-            z = self.grid[ind, 2] + np.random.randn(len(ind))
-            v = np.zeros_like(z)
-            dataset = np.stack((x, y, z, v), axis=1)
-            ida, sal_a, ind_min = self.gmrf.assimilate_data(dataset)
-            id = np.where(np.abs(z) <= -.25)[0]
-            dx = self.grid[ind_min, 0] - x[id]
-            dy = self.grid[ind_min, 1] - y[id]
-            dz = self.grid[ind_min, 2] - z[id]
-            gap = np.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
-            self.assertLess(np.amax(gap), 100)
-
-        # c3: no location
-        dataset = np.empty([0, 4])
-        ida, sal_a, ind_min = self.gmrf.assimilate_data(dataset)
-        self.assertTrue([True if len(ida) == 0 else False])
-
-    def test_get_updated_mu_mvar(self):
-        """
-        Test if the assimilation works as desired.
-        - c1: 1 value.
-        - c2: many values.
-        """
-        rotated_angle = self.gmrf.get_rotated_angle()
-
-        # c1: random location
-        x = 7000
-        y = -2000
-        depth = -.5
-        value = 10
-        dataset = np.array([[x, y, depth, value]])
-        self.gmrf.assimilate_data(dataset)
-        file = "/Users/yaolin/Downloads/"
-        self.plot_mu(file + "mu_cond")
-        self.plot_var(file + "var_cond")
-
-        # c2: desired location
-        x = 7000
-        y = -1000
-        z = -.5
-        # x = xp * np.cos(rotated_angle) + yp * np.sin(rotated_angle)
-        # y = -xp * np.sin(rotated_angle) + yp * np.cos(rotated_angle)
-        value = 15
-        dataset = np.array([[x, y, z, value]])
-        self.gmrf.assimilate_data(dataset)
-        # file = "/Users/yaoling/Downloads/"
-        self.plot_mu(file + "mu_cond2")
-        self.plot_var(file + "var_cond2")
-
-        # c3: mutiple desired location
-        x = np.array([3000, 4000, 6000])
-        y = np.array([-1500, -2000, -3000])
-        z = np.array([-1.5, -.5, -2.5])
-        # x = xp * np.cos(rotated_angle) + yp * np.sin(rotated_angle)
-        # y = -xp * np.sin(rotated_angle) + yp * np.cos(rotated_angle)
-        value = np.array([5, 10, 25])
-        dataset = np.vstack((x, y, z, value)).T
-        self.gmrf.assimilate_data(dataset)
-        # file = "/Users/yaoling/Downloads/"
-        self.plot_mu(file + "mu_cond3")
-        self.plot_var(file + "var_cond3")
+    # def test_get_updated_mu_mvar(self):
+    #     """
+    #     Test if the assimilation works as desired.
+    #     - c1: 1 value.
+    #     - c2: many values.
+    #     """
+    #     rotated_angle = self.gmrf.get_rotated_angle()
+    #
+    #     # c1: random location
+    #     x = 7000
+    #     y = -2000
+    #     depth = -.5
+    #     value = 10
+    #     dataset = np.array([[x, y, depth, value]])
+    #     self.gmrf.assimilate_data(dataset)
+    #     file = "/Users/yaolin/Downloads/"
+    #     self.plot_mu(file + "mu_cond")
+    #     self.plot_var(file + "var_cond")
+    #
+    #     # c2: desired location
+    #     x = 7000
+    #     y = -1000
+    #     z = -.5
+    #     # x = xp * np.cos(rotated_angle) + yp * np.sin(rotated_angle)
+    #     # y = -xp * np.sin(rotated_angle) + yp * np.cos(rotated_angle)
+    #     value = 15
+    #     dataset = np.array([[x, y, z, value]])
+    #     self.gmrf.assimilate_data(dataset)
+    #     # file = "/Users/yaoling/Downloads/"
+    #     self.plot_mu(file + "mu_cond2")
+    #     self.plot_var(file + "var_cond2")
+    #
+    #     # c3: mutiple desired location
+    #     x = np.array([3000, 4000, 6000])
+    #     y = np.array([-1500, -2000, -3000])
+    #     z = np.array([-1.5, -.5, -2.5])
+    #     # x = xp * np.cos(rotated_angle) + yp * np.sin(rotated_angle)
+    #     # y = -xp * np.sin(rotated_angle) + yp * np.cos(rotated_angle)
+    #     value = np.array([5, 10, 25])
+    #     dataset = np.vstack((x, y, z, value)).T
+    #     self.gmrf.assimilate_data(dataset)
+    #     # file = "/Users/yaoling/Downloads/"
+    #     self.plot_mu(file + "mu_cond3")
+    #     self.plot_var(file + "var_cond3")
 
     def plot_mu(self, filename=None):
         vmin = 10
