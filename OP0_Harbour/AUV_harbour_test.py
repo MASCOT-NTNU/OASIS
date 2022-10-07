b5cae5113070d0adca58d96c3aecf42dd8a5ead4
Yaolin Ge <geyaolin@gmail.com>
2022-10-07 12:27:13 +0200

    update_harbour_HITL_test

--- OP0_Harbour/AUV_harbour_test.py
+++ OP0_Harbour/AUV_harbour_test.py
@@ -9,7 +9,7 @@ import numpy as np
 from auv_handler import AuvHandler
 import imc_ros_interface
 from imc_ros_interface.msg import Temperature, Salinity, EstimatedState, Sms
-from math import radians
+import math
 import time
 
 """
@@ -33,11 +33,10 @@ class HarbourExample:
         rospy.Subscriber("/Vehicle/Out/Salinity_filtered", Salinity, self.SalinityCB)
         rospy.Subscriber("/Vehicle/Out/EstimatedState_filtered", EstimatedState, self.EstimatedStateCB)
 
-
-        self.speed = 1.6 #m/s
+        self.speed = .01 #m/s
         self.depth = 0.0 #meters
-        self.lat = 41.185881*np.pi/180.0
-        self.lon = -8.706053*np.pi/180.0
+        self.lat = math.radians(41.185881)
+        self.lon = math.radians(-8.706053)
         self.last_state = "unavailable"
         self.rate.sleep()
 
@@ -49,12 +48,18 @@ class HarbourExample:
         self.currentTemperature = 0.0
         self.currentSalinity = 0.0
 
-        # Pre-scripted waypoints for the AUV to move in 
-        self.waypoints = [[-8.706053,41.185881, 0],
-                        [-8.706805,41.185501, 0.5],
-                        [-8.706172,41.184888, 0],
-                        [-8.705431,41.185291, 0],
-                        [-8.706053,41.185881, 0]]
+        # Pre-scripted waypoints for the AUV to move in
+        self.waypoints = np.array([[41.178289, -8.597721, 0],
+                                   [41.176908, -8.601154, 0],
+                                   [41.175826, -8.603643, 0],
+                                   [41.174905, -8.605381, 0],
+                                   [41.173946, -8.608232, 0],
+                                   [41.173198, -8.689417, 0]])
+        # self.waypoints = [[-8.706053,41.185881, 0],
+        #                 [-8.706805,41.185501, 0.5],
+        #                 [-8.706172,41.184888, 0],
+        #                 [-8.705431,41.185291, 0],
+        #                 [-8.706053,41.185881, 0]]
 
         self.counter = 0
         self.max_count = len(self.waypoints)
@@ -64,8 +69,6 @@ class HarbourExample:
        
         self.temperature = []
         self.salinity = []
-
-
 
     def TemperatureCB(self,msg):
         self.currentTemperature = msg.value.data
@@ -78,42 +81,40 @@ class HarbourExample:
         self.lon = msg.lon.data
         self.depth = msg.depth.data
 
-
-    def send_sms(self):
-        SMS = Sms()
-        SMS = Sms()
-        SMS.number.data = self.phone_number
-        SMS.timeout.data = 60
-        SMS.contents.data = "Congrats, sms sucsess!"
-        self.sms_pub_.publish(SMS)
-        print("Finished SMS sending!")
-
-
+    # def send_sms(self):
+    #     SMS = Sms()
+    #     SMS = Sms()
+    #     SMS.number.data = self.phone_number
+    #     SMS.timeout.data = 60
+    #     SMS.contents.data = "Congrats, sms sucsess!"
+    #     self.sms_pub_.publish(SMS)
+    #     print("Finished SMS sending!")
 
 
     def run(self):
         while not rospy.is_shutdown():
             if self.init:
 
-                lat = self.lat
-                lon = self.lon
-                depth = self.depth
-                temperature = self.currentTemperature
-                time = time.time()
-                salinity = self.currentSalinity
+                # lat = self.lat
+                # lon = self.lon
+                # depth = self.depth
+                # temperature = self.currentTemperature
+                # time = time.time()
+                # salinity = self.currentSalinity
                 
-                self.dataset = np.append(self.dataset, 
-                        np.array([lat, lon, depth, salinity, temperature, time]).reshape(1, -1), axis=0)
+                # self.dataset = np.append(self.dataset,
+                #         np.array([lat, lon, depth, salinity, temperature, time]).reshape(1, -1), axis=0)
                 if self.auv_handler.getState() == "waiting":
-                    lat = self.waypoints[self.counter][0]*np.pi/180.0
-                    lon = self.waypoints[self.counter][1]*np.pi/180.0
-                    depth = self.waypoints[self.counter][2]
-                    self.auv_handler.setWaypoint(lat, lon, depth = depth)
+                    wp = self.waypoints[self.counter]
+                    # lat = self.waypoints[self.counter][0]*np.pi/180.0
+                    # lon = self.waypoints[self.counter][1]*np.pi/180.0
+                    # depth = self.waypoints[self.counter][2]
+                    self.auv_handler.setWaypoint(math.radians(wp[0]), math.radians(wp[1]), depth=wp[2])
                     self.counter += 1
 
-                    if self.counter == 2: 
-                        #send message
-                        self.auv_handler.sendSMS()
+                    # if self.counter == 2:
+                    #     #send message
+                    #     self.auv_handler.sendSMS()
                     print(self.counter)
 
                 self.last_state = self.auv_handler.getState()
