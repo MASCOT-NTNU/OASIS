296bd3f2117d8d2268a3fcd20dac4e19c52e9f01
Yaolin Ge <geyaolin@gmail.com>
2022-10-08 17:26:33 +0100

    WAITING_for_gmrf_grid_in_xy

--- OP1_SPDE3D/src/GMRF/GMRF.py
+++ OP1_SPDE3D/src/GMRF/GMRF.py
@@ -1,6 +1,8 @@
 """
 This helper solves all the essential problems associated with GMRF class.
 """
+import math
+
 from GMRF.spde import spde
 from typing import Union
 from WGS import WGS
@@ -11,17 +13,22 @@ import os
 import pandas as pd
 from usr_func.sort_polygon_vertices import sort_polygon_vertices
 from usr_func.checkfolder import checkfolder
+from usr_func.vectorize import vectorize
 import time
+
 # TODO: There are many abundant code for loading polygons and so on, need to solve that.
+# TODO: delete
+import matplotlib.pyplot as plt
+
 
 
 class GMRF:
-    __MIN_DEPTH_FOR_DATA_ASSIMILATION = -.25
-    __GMRF_DISTANCE_NEIGHBOUR = 65  # lateral distance used to scale depth comparison
+    __MIN_DEPTH_FOR_DATA_ASSIMILATION = .25
+    __GMRF_DISTANCE_NEIGHBOUR = 1  # lateral distance used to scale depth comparison
     __gmrf_grid = None
     __N_gmrf_grid = 0
     __rotated_angle = .0
-    __cnt = 0
+    __cnt_data_assimilation = 0
 
     # used for data assimilation
     __xg = None
@@ -31,7 +38,7 @@ class GMRF:
 
     def __init__(self):
         self.__spde = spde()
-        self.construct_gmrf_grid()
+        self.__construct_gmrf_grid()
         t = int(time.time())
         f = os.getcwd()
         self.foldername = f + "/GMRF/data/{:d}/".format(t)
@@ -41,7 +48,7 @@ class GMRF:
         checkfolder(self.foldername_ctd)
         checkfolder(self.foldername_thres)
 
-    def construct_gmrf_grid(self) -> None:
+    def __construct_gmrf_grid(self) -> None:
         """
         Construct GMRF grid by converting lats, lons to xy.
         """
@@ -54,28 +61,28 @@ class GMRF:
         self.__gmrf_grid = np.stack((x, y, z), axis=1)
         self.__N_gmrf_grid = self.__gmrf_grid.shape[0]
 
-        self.__xg = self.__gmrf_grid[:, 0].reshape(-1, 1)
-        self.__yg = self.__gmrf_grid[:, 1].reshape(-1, 1)
-        self.__zg = self.__gmrf_grid[:, 2].reshape(-1, 1)
+        self.__xg = vectorize(self.__gmrf_grid[:, 0])
+        self.__yg = vectorize(self.__gmrf_grid[:, 1])
+        self.__zg = vectorize(self.__gmrf_grid[:, 2])
         self.__Fgmrf = np.ones([1, self.__N_gmrf_grid])
 
-        import matplotlib.pyplot as plt
-        plt.plot(self.__gmrf_grid[:, 1], self.__gmrf_grid[:, 0], 'k.')
-        plt.show()
+        # import matplotlib.pyplot as plt
+        # plt.plot(self.__gmrf_grid[:, 1], self.__gmrf_grid[:, 0], 'k.')
+        # plt.show()
 
-        import plotly.graph_objects as go
-        import plotly
-        fig = go.Figure(data=go.Scatter3d(
-            x=self.__gmrf_grid[:, 1],
-            y=self.__gmrf_grid[:, 0],
-            z=self.__gmrf_grid[:, 2],
-            mode='markers',
-            marker=dict(
-                size=2,
-                color='black',
-            )
-        ))
-        plotly.offline.plot(fig, filename="/Users/yaolin/Downloads/test.html", auto_open=True)
+        # import plotly.graph_objects as go
+        # import plotly
+        # fig = go.Figure(data=go.Scatter3d(
+        #     x=self.__gmrf_grid[:, 1],
+        #     y=self.__gmrf_grid[:, 0],
+        #     z=self.__gmrf_grid[:, 2],
+        #     mode='markers',
+        #     marker=dict(
+        #         size=2,
+        #         color='black',
+        #     )
+        # ))
+        # plotly.offline.plot(fig, filename="/Users/yaolin/Downloads/test.html", auto_open=True)
 
         """
           Get the rotation of the grid, used for later plotting.
@@ -84,10 +91,23 @@ class GMRF:
         polygon = box[:, 2:]
         polygon = np.stack((WGS.latlon2xy(polygon[:, 0], polygon[:, 1])), axis=1)
         polygon = sort_polygon_vertices(polygon)
-        self.__rotated_angle = np.math.atan2(polygon[1, 0] - polygon[0, 0],
-                                             polygon[1, 1] - polygon[0, 1])
+        self.__rotated_angle = np.math.atan2(polygon[-1, 0] - polygon[0, 0],
+                                             polygon[-1, 1] - polygon[0, 1])
 
-        print("Hello world")
+        # plt.plot(polygon[:, 1], polygon[:, 0], 'r-.')
+        # plt.plot(polygon[0, 1], polygon[0, 0], 'r.')
+        # plt.plot(polygon[1, 1], polygon[1, 0], 'g.')
+        # plt.plot(polygon[2, 1], polygon[2, 0], 'b.')
+        # plt.plot(polygon[3, 1], polygon[3, 0], 'y.')
+        # plt.axhline(0)
+        # plt.axvline(0)
+        # plt.axvline(4800)
+        # plt.axhline(8300)
+        # plt.xlim([-10, 10])
+        # plt.ylim([8250, 8350])
+        # plt.show()
+
+        # ra = math.degrees(self.__rotated_angle)
 
     def assimilate_data(self, dataset: np.ndarray) -> tuple:
         """
@@ -98,9 +118,9 @@ class GMRF:
         """
         # ss1: save raw ctd
         df = pd.DataFrame(dataset, columns=['x', 'y', 'z', 'salinity'])
-        df.to_csv(self.foldername_ctd + "D_{:03d}.csv".format(self.__cnt))
+        df.to_csv(self.foldername_ctd + "D_{:03d}.csv".format(self.__cnt_data_assimilation))
 
-        ind_remove_noise_layer = np.where(np.abs(dataset[:, 2]) <= self.__MIN_DEPTH_FOR_DATA_ASSIMILATION)[0]
+        ind_remove_noise_layer = np.where(np.abs(dataset[:, 2]) >= self.__MIN_DEPTH_FOR_DATA_ASSIMILATION)[0]
         dataset = dataset[ind_remove_noise_layer, :]
         xd = dataset[:, 0].reshape(-1, 1)
         yd = dataset[:, 1].reshape(-1, 1)
@@ -122,14 +142,14 @@ class GMRF:
         # ss2: save assimilated data
         data = np.hstack((ind_assimilated.reshape(-1, 1), salinity_assimilated))
         df = pd.DataFrame(data, columns=['ind', 'salinity'])
-        df.to_csv(self.foldername + "D_{:03d}.csv".format(self.__cnt))
+        df.to_csv(self.foldername + "D_{:03d}.csv".format(self.__cnt_data_assimilation))
 
         # ss3: save threshold
-        # threshold = self.__spde.getThreshold()
-        # df = pd.DataFrame(threshold.reshape(1, -1), columns=['threshold'])
-        # df.to_csv(self.foldername_thres + "D_{:03d}.csv".format(self.__cnt))
+        threshold = self.__spde.threshold
+        df = pd.DataFrame(threshold.reshape(1, -1), columns=['threshold'])
+        df.to_csv(self.foldername_thres + "D_{:03d}.csv".format(self.__cnt_data_assimilation))
 
-        self.__cnt += 1
+        self.__cnt_data_assimilation += 1
         # t2 = time.time()
         # print("Data assimilation takes: ", t2 - t1)
         return ind_assimilated, salinity_assimilated, ind_min_distance
