427ea3a9eabd9f5e6c9cd6e98fda99ea015278f7
Yaolin Ge <geyaolin@gmail.com>
2022-10-09 21:29:56 +0100

    finalise_spde3d

--- OP3_Zoom_GRF/src/Penguin/GRF3D.py
+++ OP3_Zoom_GRF/src/Penguin/GRF3D.py
@@ -3,12 +3,18 @@ GRF3D module assimilates the data from the CTD measurements and then update the 
 covariance structure of the field based on Gaussian updating mechanism. It provides an easier way to
 access EIBV and other variance-related calculations.
 """
+from Penguin.WaypointGraph import WaypointGraph
+import numpy as np
 
 
 class GRF3D:
+    polygon = np.array([[0, 0],
+                        [0, 1],
+                        [1, 1],
+                        [1, 0],
+                        [0, 0]])
 
     def __init__(self):
-
         pass
 
     def assimilate_data(self):
--- OP3_Zoom_GRF/src/Penguin/WaypointGraph.py
+++ OP3_Zoom_GRF/src/Penguin/WaypointGraph.py
@@ -38,29 +38,19 @@ import os
 
 
 class WaypointGraph:
-    # Load set up parameters.
-    __waypoints = np.empty([0, 3])
-    __neighbour_distance = 120  # metres between neighbouring locations.
-    __polygon_border = np.array([[0, 0],
-                                 [0, 1000],
-                                 [1000, 1000],
-                                 [1000, 0],
-                                 [0, 0]])
-    __polygon_border_shapely = Polygon(__polygon_border)
-    __xmin, __ymin = map(np.amin, [__polygon_border[:, 0], __polygon_border[:, 1]])
-    __xmax, __ymax = map(np.amax, [__polygon_border[:, 0], __polygon_border[:, 1]])
-    __depths = np.array([-0.5, -1, -1.5, -2, -2.5])
-    __no_depth_layers = len(__depths)
-    __neighbour_hash_table = dict()
-    __ygap = __neighbour_distance * cos(radians(60)) * 2
-    __xgap = __neighbour_distance * sin(radians(60))
+    def __init__(self):
+        self.__waypoints = np.empty([0, 3])
+        self.__neighbour_distance = 120  # metres between neighbouring locations.
+        self.__polygon_border = np.array([[0, 0],
+                                          [0, 1000],
+                                          [1000, 1000],
+                                          [1000, 0],
+                                          [0, 0]])
+        self.__polygon_border_shapely = Polygon(self.__polygon_border)
+        self.__neighbour_hash_table = dict()
+        self.__depths = np.array([-0.5, -1, -1.5, -2, -2.5])
 
-    # def __init__(self):
-    #     self.construct_waypoints()
-    #     self.construct_hash_neighbours()
-
-    @staticmethod
-    def construct_waypoints() -> None:
+    def construct_waypoints(self) -> None:
         """ Construct the waypoint graph based on the instruction given above.
         - Construct regular meshgrid.
         .  .  .  .
@@ -73,34 +63,39 @@ class WaypointGraph:
         - Then remove illegal locations.
         - Then add the depth layers.
         """
-        gx = np.arange(WaypointGraph.__xmin, WaypointGraph.__xmax, WaypointGraph.__xgap)  # get [0, x_gap, 2*x_gap, ..., (n-1)*x_gap]
-        gy = np.arange(WaypointGraph.__ymin, WaypointGraph.__ymax, WaypointGraph.__ygap)
+        self.__waypoints = np.empty([0, 3])
+        xmin, ymin = map(np.amin, [self.__polygon_border[:, 0], self.__polygon_border[:, 1]])
+        xmax, ymax = map(np.amax, [self.__polygon_border[:, 0], self.__polygon_border[:, 1]])
+        ygap = self.__neighbour_distance * cos(radians(60)) * 2
+        xgap = self.__neighbour_distance * sin(radians(60))
+        self.__no_depth_layers = len(self.__depths)
+
+        gx = np.arange(xmin, xmax, xgap)  # get [0, x_gap, 2*x_gap, ..., (n-1)*x_gap]
+        gy = np.arange(ymin, ymax, ygap)
         grid2d = []
         counter_grid2d = 0
         for i in range(len(gy)):
             for j in range(len(gx)):
                 if j % 2 == 0:
                     x = gx[j]
-                    y = gy[i] + WaypointGraph.__ygap / 2
+                    y = gy[i] + ygap / 2
                 else:
                     x = gx[j]
                     y = gy[i]
                 p = Point(x, y)
-                if WaypointGraph.__border_contains(p):
+                if self.__border_contains(p):
                     grid2d.append([x, y])
                     counter_grid2d += 1
-        WaypointGraph.multiple_depth_layer = False
-        if WaypointGraph.__no_depth_layers > 1:
-            WaypointGraph.multiple_depth_layer = True
-        for i in range(WaypointGraph.__no_depth_layers):
+        self.multiple_depth_layer = False
+        if self.__no_depth_layers > 1:
+            self.multiple_depth_layer = True
+        for i in range(self.__no_depth_layers):
               for j in range(counter_grid2d):
-                WaypointGraph.__waypoints = np.append(WaypointGraph.__waypoints,
-                                                      np.array([grid2d[j][0], grid2d[j][1],
-                                                                WaypointGraph.__depths[i]]).reshape(1, -1), axis=0)
-        WaypointGraph.__waypoints = np.array(WaypointGraph.__waypoints)
+                self.__waypoints = np.append(self.__waypoints, np.array([grid2d[j][0], grid2d[j][1],
+                                                                         self.__depths[i]]).reshape(1, -1), axis=0)
+        self.__waypoints = np.array(self.__waypoints)
 
-    @staticmethod
-    def construct_hash_neighbours() -> None:
+    def construct_neighbour_hash_table(self) -> None:
         """ Construct the hash table for containing neighbour indices around each waypoint.
         - Get the adjacent depth layers
             - find the current depth layer index, then find the upper and lower depth layer indices.
@@ -109,88 +104,75 @@ class WaypointGraph:
         - Append all the neighbour indices for each waypoint.
         """
         # check adjacent depth layers to determine the neighbouring waypoints.
-        WaypointGraph.no_waypoint = WaypointGraph.__waypoints.shape[0]
+        self.no_waypoint = self.__waypoints.shape[0]
         ERROR_BUFFER = 1
-        for i in range(WaypointGraph.no_waypoint):
+        for i in range(self.no_waypoint):
             # determine ind depth layer
-            xy_c = WaypointGraph.__waypoints[i, 0:2].reshape(1, -1)
-            d_c = WaypointGraph.__waypoints[i, 2]
-            ind_d = np.where(WaypointGraph.__depths == d_c)[0][0]
+            xy_c = self.__waypoints[i, 0:2].reshape(1, -1)
+            d_c = self.__waypoints[i, 2]
+            ind_d = np.where(self.__depths == d_c)[0][0]
 
             # determine ind adjacent layers
-            ind_u = ind_d + 1 if ind_d < WaypointGraph.__no_depth_layers - 1 else ind_d
+            ind_u = ind_d + 1 if ind_d < self.__no_depth_layers - 1 else ind_d
             ind_l = ind_d - 1 if ind_d > 0 else 0
 
             # compute lateral distance
             id = np.unique([ind_d, ind_l, ind_u])
-            ds = WaypointGraph.__depths[id]
+            ds = self.__depths[id]
 
             ind_n = []
             for ids in ds:
-                ind_id = np.where(WaypointGraph.__waypoints[:, 2] == ids)[0]
-                xy = WaypointGraph.__waypoints[ind_id, 0:2]
+                ind_id = np.where(self.__waypoints[:, 2] == ids)[0]
+                xy = self.__waypoints[ind_id, 0:2]
                 dist = cdist(xy, xy_c)
-                ind_n_temp = np.where((dist <= WaypointGraph.__neighbour_distance + ERROR_BUFFER) *
-                                      (dist >= WaypointGraph.__neighbour_distance - ERROR_BUFFER))[0]
+                ind_n_temp = np.where((dist <= self.__neighbour_distance + ERROR_BUFFER) *
+                                      (dist >= self.__neighbour_distance - ERROR_BUFFER))[0]
                 for idt in ind_n_temp:
                     ind_n.append(ind_id[idt])
-            WaypointGraph.__neighbour_hash_table[i] = ind_n
+            self.__neighbour_hash_table[i] = ind_n
 
-    @staticmethod
-    def set_neighbour_distance(value: float) -> None:
+    def set_neighbour_distance(self, value: float) -> None:
         """ Set the neighbour distance """
-        WaypointGraph.__neighbour_distance = value
+        self.__neighbour_distance = value
 
-    @staticmethod
-    def set_depth_layers(value: np.ndarray) -> None:
+    def set_depth_layers(self, value: np.ndarray) -> None:
         """ Set the depth layers as np.array([-.5, -1.5, -2.5]) """
-        WaypointGraph.__depths = value
+        self.__depths = value
+        self.__no_depth_layers = len(self.__depths)
 
-    @staticmethod
-    def set_polygon_border(value: np.ndarray) -> None:
+    def set_polygon_border(self, value: np.ndarray) -> None:
         """ Set the polygon border, only one Nx2 dimension allowed """
-        WaypointGraph.__polygon_border = value
-        WaypointGraph.__polygon_border_shapely = Polygon(WaypointGraph.__polygon_border)
+        self.__polygon_border = value
+        self.__polygon_border_shapely = Polygon(self.__polygon_border)
 
-    @staticmethod
-    def get_neighbour_distance() -> float:
-        return WaypointGraph.__neighbour_distance
+    def get_neighbour_distance(self) -> float:
+        return self.__neighbour_distance
 
-    @staticmethod
-    def get_depth_layers() -> np.ndarray:
-        return WaypointGraph.__depths
+    def get_depth_layers(self) -> np.ndarray:
+        return self.__depths
 
-    @staticmethod
-    def get_polygon_border() -> np.ndarray:
-        return WaypointGraph.__polygon_border
+    def get_polygon_border(self) -> np.ndarray:
+        return self.__polygon_border
 
-    @staticmethod
-    def get_polygon_border_shapely() -> 'Polygon':
-        return WaypointGraph.__polygon_border_shapely
+    def get_polygon_border_shapely(self) -> 'Polygon':
+        return self.__polygon_border_shapely
 
-    @staticmethod
-    def get_waypoints() -> np.ndarray:
-        """
-        Returns: waypoints
-        """
-        return WaypointGraph.__waypoints
+    def get_waypoints(self) -> np.ndarray:
+        return self.__waypoints
 
-    @staticmethod
-    def get_neighbour_hash_table():
+    def get_neighbour_hash_table(self) -> dict:
         """
         Returns: neighbour hash table
         """
-        return WaypointGraph.__neighbour_hash_table
+        return self.__neighbour_hash_table
 
-    @staticmethod
-    def get_waypoint_from_ind(ind: Union[int, list, np.ndarray]) -> np.ndarray:
+    def get_waypoint_from_ind(self, ind: Union[int, list, np.ndarray]) -> np.ndarray:
         """
         Return waypoint locations using ind.
         """
-        return WaypointGraph.__waypoints[ind, :]
+        return self.__waypoints[ind, :]
 
-    @staticmethod
-    def get_ind_from_waypoint(waypoint: np.ndarray) -> Union[int, np.ndarray, None]:
+    def get_ind_from_waypoint(self, waypoint: np.ndarray) -> Union[int, np.ndarray, None]:
         """
         Args:
             waypoint: np.array([xp, yp, zp])
@@ -200,27 +182,25 @@ class WaypointGraph:
         if len(waypoint) > 0:
             dm = waypoint.ndim
             if dm == 1:
-                d = cdist(WaypointGraph.__waypoints, waypoint.reshape(1, -1))
+                d = cdist(self.__waypoints, waypoint.reshape(1, -1))
                 return np.argmin(d, axis=0)[0]
             elif dm == 2:
-                d = cdist(WaypointGraph.__waypoints, waypoint)
+                d = cdist(self.__waypoints, waypoint)
                 return np.argmin(d, axis=0)
             else:
                 return None
         else:
             return None
 
-    @staticmethod
-    def get_neighbour_indices(ind: int) -> list:
+    def get_neighbour_indices(self, ind: int) -> list:
         """
         Args:
             ind: waypoint index
         Returns: neighbour indices
         """
-        return WaypointGraph.__neighbour_hash_table[ind]
+        return self.__neighbour_hash_table[ind]
 
-    @staticmethod
-    def get_vector_between_two_waypoints(wp1: np.ndarray, wp2: np.ndarray) -> np.ndarray:
+    def get_vector_between_two_waypoints(self, wp1: np.ndarray, wp2: np.ndarray) -> np.ndarray:
         """ Get a vector from wp1 to wp2.
 
         Args:
@@ -239,10 +219,9 @@ class WaypointGraph:
         vec = np.vstack((dx, dy, dz))
         return vec
 
-    @staticmethod
-    def __border_contains(point: Point) -> bool:
+    def __border_contains(self, point: Point) -> bool:
         """ Test if point is within the border polygon """
-        return WaypointGraph.__polygon_border_shapely.contains(point)
+        return self.__polygon_border_shapely.contains(point)
 
 
 if __name__ == "__main__":
