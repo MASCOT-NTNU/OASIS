c8377d3fa03f8c27373026e729ada72a041152b3
Yaolin Ge <geyaolin@gmail.com>
2022-10-09 20:03:24 +0200

    update_waypoint_graph

--- OP3_Zoom_GRF/src/Penguin/GRF3D.py
+++ OP3_Zoom_GRF/src/Penguin/GRF3D.py
@@ -1,14 +1,24 @@
 """
-GRF3D
-
+GRF3D module assimilates the data from the CTD measurements and then update the conditional mean and
+covariance structure of the field based on Gaussian updating mechanism. It provides an easier way to
+access EIBV and other variance-related calculations.
 """
 
 
 class GRF3D:
 
     def __init__(self):
+
         pass
 
+    def assimilate_data(self):
+        pass
+
+    def update(self):
+        pass
+
+    def get_eibv_at_location(self):
+        pass
 
 
 if __name__ == "__main__":
